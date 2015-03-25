1cc276c0079df45eb53a6bd45944bce32a879bf7
yemengying <391859458@qq.com>
2015-03-25 15:26:24 +0800

    add comment

--- routes/project.js
+++ routes/project.js
@@ -14,32 +14,33 @@ router.get('/', function (req, res) {
     var map = new hashMap();
     db.getConnection(function (err, conn) {
         if (err) console.log("POOL ==> " + err);
+        //从project_user_info视图中获取user和project的信息，user未报名的项目不存储在数据库中
         db.query('SELECT projectId,projectName,categoryName,creatorName,people,content,projectStatus,memberStatus ' +
         'FROM project_user_info WHERE userToken = ' + token + ' ',function(err,rows){
             if(err){
                 console.log(err);
                 result = err;
-                res.send(false);
+                res.send(false);//若有错误返回失败
                 conn.release();
             }else{
                 for(var i in rows){
                     project= new projectUserModel( rows[i].projectName,rows[i].categoryName,rows[i].creatorName,rows[i].people,
                         rows[i].content,rows[i].memberStatus,rows[i].projectStatus);
+                    //把取出结果以projectId为key 加入hashmap中
                     map.set(rows[i].projectId, project);
                 }
+                //取出所有项目信息
                 db.query('SELECT * FROM project_info',function(err,rows){
                     for(var i in rows){
+                        //若以项目id为key 无法从map中取出对象 说明指定用户未报名该项目
                         if(map.get(rows[i].projectId) ===  undefined){
+                            //新建project对象，用户状态为0，说明未报名
                             project = new projectUserModel( rows[i].projectName,rows[i].categoryName,rows[i].creatorName,rows[i].people,
                                 rows[i].content,0,rows[i].projectStatus);
-                            console.log(rows[i].projectId);
-                            console.log(project);
                             map.set(rows[i].projectId, project);
                         }
                     }
                     map.forEach(function(value,key){
-                        console.log(key + "ss"
-                        + value);
                         result.push(value);
                     })
                     res.send(result);
