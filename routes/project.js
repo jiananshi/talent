863d6db1a920ebd3d025a26a60f0455ad2e91bf2
yemengying <391859458@qq.com>
2015-04-09 22:26:27 +0800

    修改获取项目信息bug

--- routes/project.js
+++ routes/project.js
@@ -17,48 +17,58 @@ router.get('/', function (req, res) {
     var map = new hashMap();
     db.getConnection(function (err, conn) {
         if (err) console.log("POOL ==> " + err);
-        //从project_user_info视图中获取user和project的信息，user未报名的项目不存储在数据库中
-        db.query('SELECT projectId,projectName,categoryName,creatorName,people,content,projectStatus,memberStatus ' +
-        'FROM project_user_info WHERE userToken = ' + token + ' ',function(err,rows){
-            if(err){
-                console.log(err);
-                data.message = err;
-                res.send(data);//若有错误返回失败
+
+        db.query('SELECT * FROM user WHERE user_Token = '+token+'',function(err,row){
+            if(row.length == 0){
+                data.message = "重新登录" ;
+                res.send(data);
                 conn.release();
             }else{
-                for(var i in rows){
-                    project= new projectUserModel( rows[i].projectId,rows[i].projectName,rows[i].categoryName,rows[i].creatorName,rows[i].people,
-                        rows[i].content,rows[i].memberStatus,rows[i].projectStatus);
-                    //把取出结果以projectId为key 加入hashmap中
-                    map.set(rows[i].projectId, project);
-                }
-                //取出所有项目信息
-                db.query('SELECT * FROM project_info',function(err,rows){
+                //从project_user_info视图中获取user和project的信息，user未报名的项目不存储在数据库中
+                db.query('SELECT projectId,projectName,categoryName,creatorName,people,content,projectStatus,memberStatus ' +
+                'FROM project_user_info WHERE userToken = ' + token + ' ',function(err,rows){
                     if(err){
+                        console.log(err);
                         data.message = err;
                         res.send(data);//若有错误返回失败
                         conn.release();
-                    }else {
-                        for (var i in rows) {
-                            //若以项目id为key 无法从map中取出对象 说明指定用户未报名该项目
-                            if (map.get(rows[i].projectId) === undefined) {
-                                //新建project对象，用户状态为0，说明未报名
-                                project = new projectUserModel(rows[i].projectId,rows[i].projectName, rows[i].categoryName, rows[i].creatorName, rows[i].people,
-                                    rows[i].content, 0, rows[i].projectStatus);
-                                map.set(rows[i].projectId, project);
-                            }
+                    }else{
+                        for(var i in rows){
+                            project= new projectUserModel( rows[i].projectId,rows[i].projectName,rows[i].categoryName,rows[i].creatorName,rows[i].people,
+                                rows[i].content,rows[i].memberStatus,rows[i].projectStatus);
+                            //把取出结果以projectId为key 加入hashmap中
+                            map.set(rows[i].projectId, project);
                         }
-                        //将hashmap中的所有值加入result数组中
-                        map.forEach(function (value, key) {
-                            result.push(value);
+                        //取出所有项目信息
+                        db.query('SELECT * FROM project_info',function(err,rows){
+                            if(err){
+                                data.message = err;
+                                res.send(data);//若有错误返回失败
+                                conn.release();
+                            }else {
+                                for (var i in rows) {
+                                    //若以项目id为key 无法从map中取出对象 说明指定用户未报名该项目
+                                    if (map.get(rows[i].projectId) === undefined) {
+                                        //新建project对象，用户状态为0，说明未报名
+                                        project = new projectUserModel(rows[i].projectId,rows[i].projectName, rows[i].categoryName, rows[i].creatorName, rows[i].people,
+                                            rows[i].content, 0, rows[i].projectStatus);
+                                        map.set(rows[i].projectId, project);
+                                    }
+                                }
+                                //将hashmap中的所有值加入result数组中
+                                map.forEach(function (value, key) {
+                                    result.push(value);
+                                })
+                                res.send(result);
+                                conn.release();
+                            }
                         })
-                        res.send(result);
-                        conn.release();
-                    }
-                })
 
+                    }
+                });
             }
-        });
+        })
+
     });
 });
 
