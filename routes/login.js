22b03bb0b9e6b0e288fbda38d8847c8d2ffadd75
yemengying <391859458@qq.com>
2015-04-12 11:48:26 +0800

    修改loginapi

--- routes/login.js
+++ routes/login.js
@@ -8,26 +8,32 @@ var common = require('../model/common');
 router.get('/',function(req,res) {
     var db = req.db;
     var data = {
-        status: false,
-        message: ""
+        data: {
+            status: false,
+            message: ""
+        }
+    }
+    var result = {
+        token : ""
     }
     var name = req.query.name;
     var password = req.query.password;
     var token =common.sha1(name + common.random + password + new Date());
     db.getConnection(function(err,conn){
         if(err){
-            data.message = err;
-            response.send(data);
+            data.data.message = err;
+            res.send(data);
         }else{
             db.query('UPDATE user set user_token =" '+ token +'" where user_name = "'+ name+'" and user_password ="'+ password+'" ',function(err,row){
                     if(err){
-                        data.message = err;
+                        data.data.message = err;
                         response.send(data);
                     }else{
                         if(row.affectedRows === 1){
-                            res.send(token);
+                            result.token = token;
+                            res.send(result);
                         }else{
-                            data.message = "用户名或密码不正确";
+                            data.data.message = "用户名或密码不正确";
                             res.send(data);
                         }
                     }
