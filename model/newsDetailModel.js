9662739ca659f95573d97a7400dd9663b1727352
yemengying <391859458@qq.com>
2015-04-21 12:46:59 +0800

    获取新闻和通知的详细信息

--- model/newsDetailModel.js
+++ model/newsDetailModel.js
@@ -1,11 +1,14 @@
-//对应新闻的model类
-module.exports =  function(id, name ,category,imgUrl, sortContent, time, readTimes){
+/**
+ * Created by apple on 2015/4/21.
+ */
+//对应新闻详情的model类
+module.exports =  function(id, name ,category,imgUrl, content, time, readTimes){
     var news = {
         id : 0, //新闻的id
         name : "", //新闻的标题
         category : 1 ,//1.新闻 2.通知
         imgUrl : "", //图片路径
-        sortContent : "", //新闻内容
+        content : "", //新闻内容
         time : "", //创建时间
         readTimes : 0 //阅读次数
     }
@@ -13,7 +16,7 @@ module.exports =  function(id, name ,category,imgUrl, sortContent, time, readTim
     news.name = name;
     news.category = category;
     news.imgUrl = imgUrl;
-    news.sortContent = sortContent;
+    news.content = content;
     news.time = time;
     news.readTimes = readTimes;
 
--- routes/news.js
+++ routes/news.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var newsModel = require('../model/newsModel')
+var newsDetailModel = require('../model/newsDetailModel');
 
-//得到所有状态为已发布的新闻的信息(包括id，name，imgurl，time，readtime)
+//得到所有状态为已发布的新闻/通知的简单信息(包括id，name，imgurl，time，readtime)
 router.get('/', function (req, res) {
     var db = req.db;
     var data = {
@@ -49,7 +50,51 @@ router.get('/', function (req, res) {
     })
 });
 
-router.post('/', function (req, res) {
+router.get('/new-detail', function (req, res) {
+    var db = req.db;
+    var id = req.query.id;
+    var category = req.query.category;
+    var data = {
+        status : false,
+        message :""
+    }
+    var news;
+    db.getConnection(function(err,conn){
+        if(err){
+            data.message = err;
+            res.send({'data' : data});
+            conn.release();
+        }else{
+            if(category == 1 ){
+                db.query('SELECT news_id as id, news_title as name,news_show_picture as imgUrl,news_content as content,news_createtime as time,news_readtimes as readTimes ' +
+                ' From news WHERE news_id = '+id+' ', function(err,rows){
+                    if(err){
+                        console.log(err);
+                        data.message = err;
+                        res.send({'data' : data});
+                        conn.release();
+                    }else{
+                         news = newsDetailModel(rows[0].id, rows[0].name,1,rows[0].imgUrl,rows[0].content,rows[0].time,rows[0].readTimes);
+                        res.send(news);
+                        conn.release();
+                    }
+            })
+         }else{
+                db.query('SELECT notice_id as id,notice_title as name,notice_content as content,notice_createtime as time FROM notice WHERE notice_id = '+id+'', function(err,rows){
+                    if(err){
+                        console.log(err);
+                        data.message = err;
+                        res.send({'data' : data});
+                        conn.release();
+                    }else{
+                         news = newsDetailModel(rows[0].id, rows[0].name,2,"",rows[0].content,rows[0].time,"");
+                        res.send(news);
+                        conn.release();
+                    }
+                })
+            }
+        }
+    })
 });
 
 module.exports = router;
